7822b5f8360b37fdf4c6d059fa365782db21d1eb
Daiwei Lu <daiweilu123@gmail.com>
2013-04-02 12:24:32 -0500

    home_timleine

--- lib/oauth_twitter/api/statuses.rb
+++ lib/oauth_twitter/api/statuses.rb
@@ -1,6 +1,22 @@
 module OauthTwitter
   module API
     module Statuses
+      PATH = {
+        home_timeline: "/1.1/statuses/home_timeline.json"
+      }
+      
+      def home_timeline(since_id=nil)
+        oauth = oauth_params(true)
+        query = {
+          count: 200,
+          trim_user: false,
+          exclude_replies: true,
+          contributor_details: false,
+          include_entities: true
+        }
+        query[:since_id] = since_id if since_id
+        return send_request(:GET, PATH[:home_timeline], query, oauth)
+      end
       
     end
   end
