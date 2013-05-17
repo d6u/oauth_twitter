dbcddaf9df332735c95846af38592fddbf81f518
Daiwei Lu <daiweilu123@gmail.com>
2013-05-16 22:59:55 -0400

    oauth API

--- lib/oauth_twitter/api/oauth.rb
+++ lib/oauth_twitter/api/oauth.rb
@@ -2,21 +2,38 @@ module OauthTwitter
   module API
     module Oauth
       PATH = {
-        request_token: '/oauth/request_token',
-         access_token: '/oauth/access_token'
+        :oauth_authenticate => '/oauth/authenticate',
+        :oauth_authorize => '/oauth/authorize',
+        :oauth_access_token => '/oauth/access_token',
+        :oauth_request_token => '/oauth/request_token'
       }
 
-      def request_token(oauth_callback=nil)
-        callback_url = oauth_callback || Config.oauth_callback
-        oauth = oauth_params(
-          false,
-          {oauth_callback: callback_url})
-        return send_request(:POST, PATH[:request_token], nil, oauth)
+      def oauth_authenticate(params={}, options={})
+        query = params.clone
+        oauth = oauth_params(true)
+        response = send_request(:GET, PATH[:oauth_authenticate], query, oauth)
+        return results_with_error_explained(response, options)
       end
 
-      def access_token(oauth_verifier)
+      def oauth_authorize(params={}, options={})
+        query = params.clone
         oauth = oauth_params(true)
-        return send_request(:POST, PATH[:access_token], {oauth_verifier: oauth_verifier}, oauth)
+        response = send_request(:GET, PATH[:oauth_authorize], query, oauth)
+        return results_with_error_explained(response, options)
+      end
+
+      def oauth_access_token(params={}, options={})
+        query = params.clone
+        oauth = oauth_params(true)
+        response = send_request(:POST, PATH[:oauth_access_token], query, oauth)
+        return results_with_error_explained(response, options)
+      end
+
+      def oauth_request_token(params={}, options={})
+        callback_url = params[:oauth_callback] || Config.oauth_callback
+        oauth = oauth_params(false, {oauth_callback: callback_url})
+        response = send_request(:POST, PATH[:oauth_request_token], nil, oauth)
+        return results_with_error_explained(response, options)
       end
 
     end
